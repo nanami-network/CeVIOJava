50317c90b88ac5f85b16db1e09b100a6c0940734
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2022-12-24 09:06:43 +0900

    Fix Typo

--- src/main/java/xyz/n7mn/dev/data/TalkerComponentCollection.java
+++ src/main/java/xyz/n7mn/dev/data/TalkerComponentCollection.java
@@ -20,7 +20,7 @@ public class TalkerComponentCollection {
         return components.get(id);
     }
 
-    public Map<String, TalkerComponent> getAsList() {
+    public Map<String, TalkerComponent> getAsMap() {
         return components;
     }
 
